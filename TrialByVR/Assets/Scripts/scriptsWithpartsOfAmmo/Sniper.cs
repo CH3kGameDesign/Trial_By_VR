e9ae6a8777f28a8c82498d37fa615f284b07aa90
zack <zedoov@hotmail.com>
2019-06-06 16:43:06 +1000

    added scripts that have parts of ammo count
    sniper script holds the value of the ammo and world ui writes it to the text in game

---
+++ TrialByVR/Assets/Scripts/scriptsWithpartsOfAmmo/Sniper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject cameraObj;
+
+    public ParticleSystem particleToSeeEnemy;
+
+    public float ammoCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleToSeeEnemy.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && GameObject.FindGameObjectsWithTag("Bullet").Length == 0 && ammoCount > 0)
+        {
+            Instantiate(bullet, this.transform.position, cameraObj.transform.rotation);
+            ammoCount -= 1;
+        }
+
+        if (Input.anyKeyDown && GameObject.FindGameObjectsWithTag("Bullet").Length == 0)
+        {
+            Instantiate(bullet, this.transform.position, cameraObj.transform.rotation);
+            ammoCount -= 1;
+        }
+
+        if(Input.GetKey(KeyCode.Y))
+        {
+            if (!particleToSeeEnemy.isPlaying)
+            {
+                particleToSeeEnemy.Play();
+            }
+        }
+        else
+        {
+            particleToSeeEnemy.Stop();
+        }
+    }
+}
---
+++ TrialByVR/Assets/Scripts/scriptsWithpartsOfAmmo/worldUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class worldUI : MonoBehaviour
+{
+    public float timerLength;
+    public Text timer;
+    public Text ammoCount;
+
+    public Sniper ammo;
+
+    private float maxAmmo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer.text = timerLength.ToString();
+        maxAmmo = ammo.ammoCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerLength > 0)
+        {
+            timerLength -= Time.deltaTime;
+        }
+        else
+        {
+            timerLength = 0;
+        }
+        timer.text = timerLength.ToString("f2");
+        
+        ammoCount.text = ammo.ammoCount.ToString() + " / " + maxAmmo;
+    }
+}
