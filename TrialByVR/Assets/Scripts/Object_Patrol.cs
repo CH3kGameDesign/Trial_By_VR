667925eda54eb52b2191fb2bf4d29b9d9ad1d11d
Hoson <hoson_zhong@hotmail.com>
2019-06-06 11:33:14 +1000

    Patrol scripted finished

--- TrialByVR/Assets/Scripts/Object_Patrol.cs
+++ TrialByVR/Assets/Scripts/Object_Patrol.cs
@@ -1,21 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof (Rigidbody))]
 public class Object_Patrol : MonoBehaviour
 {
-    [SerializeField] private float patrolSpeed;
-    [SerializeField] private Transform[] patrolPoints;
-
+    [SerializeField, Tooltip("Speed of the patrolling object.")]
+    private float moveForce = 100;
+    [SerializeField, Tooltip("Object's rotation speed.")]
+    private int rotationSpeed = 5;
+    [SerializeField, Tooltip("The distance from its target node before it turns.")]
     private float arrival_distance = 0.5f;
-    private float lerp_time = 0.5f;
+    [SerializeField, Tooltip("Transforms of the points you want the object to follow.")]
+    private Transform[] patrolPoints;
 
     private Rigidbody rigid_body = null;
     private int node_index = 0;
-
-    private Vector3 start_velocity;
-    private float lerp_timer;
 
     // Start is called before the first frame update
     void Start()
@@ -26,35 +24,24 @@ public class Object_Patrol : MonoBehaviour
         transform.position = patrolPoints[0].position;
 
         rigid_body = GetComponent<Rigidbody>();
-
-        lerp_timer = lerp_time;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (patrolPoints.Length <= 0 || rigid_body == null)
+        if (patrolPoints.Length < 2 || rigid_body == null)
             return;
 
-        Vector3 directional_force = (patrolPoints[node_index].position - transform.position).normalized;
-        directional_force *= patrolSpeed + Time.deltaTime;
+        Vector3 direction = patrolPoints[node_index].position - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
 
-        if (lerp_timer < lerp_time)
-        {
-            rigid_body.velocity = Vector3.Slerp(start_velocity, directional_force, lerp_timer / lerp_time);
-        }
-
-
-        rigid_body.velocity = directional_force;
+        rigid_body.velocity = transform.forward * moveForce * Time.deltaTime;
 
         if (Vector3.Distance(transform.position, patrolPoints[node_index].position) <= arrival_distance)
         {
             ++node_index;
             // Wrap array index once it goes out of range
             node_index = (node_index % patrolPoints.Length + patrolPoints.Length) % patrolPoints.Length;
-
-            start_velocity = rigid_body.velocity;
-            lerp_timer = 0;
         }
     }
 }
